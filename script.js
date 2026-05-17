0a55fe08eb8f2aced20b0eb34c52ad253e1004d7
Nitish kumar <avdeshkumar4489@gmail.com>
2026-05-17 20:31:18 +0530

    Refactor script.js to include LuxeuraApp class

--- script.js
+++ script.js
@@ -25,3 +25,42 @@ function scrollTofooter(){
         behavior:"smooth"
     });
 }
+
+class LuxeuraApp {
+    constructor() {
+        this.cart = JSON.parse(localStorage.getItem('luxeura-cart')) || [];
+        this.isDarkMode = localStorage.getItem('luxeura-theme') !== 'light';
+        this.init();
+    }
+
+    init() {
+        this.initLoader();
+        this.initCursor();
+        this.initNavbar();
+        this.initScrollAnimations();
+        this.initProductFilters();
+        this.initSliders();
+        this.initGallery();
+        this.initModals();
+        this.initForms();
+        this.initThemeToggle();
+        this.populateProducts();
+        this.populateSlider();
+        this.populateReviews();
+        this.updateCartCount();
+    }
+
+    initLoader() {
+        const loader = document.querySelector('.loader');
+        setTimeout(() => {
+            loader.style.opacity = '0';
+            setTimeout(() => loader.remove(), 500);
+        }, 2500);
+    }
+}
+
+let app;
+document.addEventListener('DOMContentLoaded', () => {
+    app = new LuxeuraApp();
+    app.smoothScroll();
+});
